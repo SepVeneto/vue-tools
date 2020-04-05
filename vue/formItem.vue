9283654475a185979daed595399760a658a659d9
SepVeneto <245114264@qq.com>
2020-04-05 18:49:22 +0800

    Add files via upload

---
+++ vue/formItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-col :span="span">
+    <el-form-item v-bind="$attrs">
+      <slot />
+    </el-form-item>
+  </el-col>
+</template>
+
+<script>
+export default {
+  name: 'form-item',
+  functional: true,
+  props: {
+    span: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
